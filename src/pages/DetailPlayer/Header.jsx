46a014b69a8088b32e42f2314d7ef07dbdcd977e
mchis1908 <mchis19.08@gmail.com>
2022-11-03 18:55:54 +0700

    updated

--- src/pages/DetailPlayer/Header.jsx
+++ src/pages/DetailPlayer/Header.jsx
@@ -7,19 +7,19 @@ import hagl from '../images/hagl.png'
 export default function Header() {
   return (
     <div className='header_detailplayer'>
-    <div className='img1'>
-        <img src={hagl} alt='hagl' style={{paddingTop:'100px'}} width='60px' height='170px' className='img' />
-    </div>
-    <div className='img2'>
-        <img src={cp} alt='cp' width='300px' height='180px' className='img' />
-    </div>
-    <div className='info'>
-        <p>Nguyễn Công Phượng</p>
-        <p className='detail_player_role'>Tiền đạo</p>
-    </div>
-    <div className='numberbig'>
-        <p>10</p>
-    </div>
+      <div className='img1'>
+          <img src={hagl} alt='hagl' style={{paddingTop:'100px'}} width='60px' height='170px' className='img' />
+      </div>
+      <div className='img2'>
+          <img src={cp} alt='cp' width='300px' height='180px' className='img' />
+      </div>
+      <div className='info'>
+          <p>Nguyễn Công Phượng</p>
+          <p className='detail_player_role'>Tiền đạo</p>
+      </div>
+      <div className='numberbig'>
+          <p>10</p>
+      </div>
     </div>
   )
 }
